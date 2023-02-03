1905e863606adeb4c997f0d98d42ca8b046b98f4
kes09 <manjgaladzek00@gmail.com>
2023-02-03 11:09:16 +0400

    modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
+
+  return(
+    <Child isUserLoggedIn={true}  const users = {[
+      {
+        id:1,
+        name:"Robert",
+        lname:" Kiyosaki",
+        age:"63"
+      },
+      {
+        id:2,
+        name:"Chethan",
+        lname:" bhagath",
+        age:"44"
+      },
+      {
+        id:3,
+        name:"Napoleon",
+        lname:" Hill",
+        age:"89"
+      },
+      {
+        id:4,
+        name:"Ankur",
+        lname:"Warikoo",
+        age:"33"
+      }
+    ]} />
+  )
+}
+const Child =({users,isUserLoggedIn})=>{
+  if (users.length === 0) {
+    return <h2>No user in System</h2>
+  }
+  if (isUserLoggedIn === false) {
+    return <h2>are not authorized to see user list.</h2>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      {users.map((arr) => (
+             <h1 key={arr.id}>
+              Name:{arr.name} - Surname:{arr.lname} - Age:{arr.age}
+          </h1>
+        ))}
+    </>
   );
 }
 
